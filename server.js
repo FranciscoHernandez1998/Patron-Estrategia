a7893c94a0ed2698ba7744bab9a2464c023164e6
Francisco <fjch87@gmail.com>
2020-10-04 13:01:05 -0600

    Animaciones

--- server.js
+++ server.js
@@ -43,37 +43,37 @@ function crearEstrategia(elemento){
     if(elemento==='aire'){
         estrategia = {
             ejecutarAccionDeEstrategia: function (res) {
-                res.send({info:'Aang está usando aire control',img:'/images/aire_control.jpg'});
+                res.send({info:'Aang está usando aire control',img:'/images/aire_control.gif'});
             }
         }
     }else if(elemento==='agua'){
         estrategia = {
             ejecutarAccionDeEstrategia: function (res) {
-                res.send({info:'Aang está usando agua control',img:'/images/agua_control.png'});
+                res.send({info:'Aang está usando agua control',img:'/images/agua_control.gif'});
             }
         }
     }else if(elemento==='fuego'){
         estrategia = {
             ejecutarAccionDeEstrategia: function (res) {
-                res.send({info:'Aang está usando fuego control',img:'/images/fuego_control.jpg'});
+                res.send({info:'Aang está usando fuego control',img:'/images/fuego_control.gif'});
             }
         }
     }else if(elemento==='tierra'){
         estrategia = {
             ejecutarAccionDeEstrategia: function (res) {
-                res.send({info:'Aang está usando tierra control',img:'/images/tierra_control.jpg'});
+                res.send({info:'Aang está usando tierra control',img:'/images/tierra_control.gif'});
             }
         }
     }else if(elemento==='modo_avatar'){
         estrategia = {
             ejecutarAccionDeEstrategia: function (res) {
-                res.send({info:'Aang está en modo avatar',img:'/images/modo_avatar.jpg'});
+                res.send({info:'Aang está en modo avatar',img:'/images/modo_avatar.gif'});
             }
         }
     }else if(elemento==='preparado'){
         estrategia = {
             ejecutarAccionDeEstrategia: function (res) {
-                res.send({info:'Aang está preparado',img:'/images/normal_aang.png'});
+                res.send({info:'Aang está preparado',img:'/images/aang_preparado.gif'});
             }
         }
     }
